7915dd5f9197e78ad165584c375499f5ddace530
JDaiNCK777 <juy62610@liceo.edu.ph>
2026-03-31 13:14:35 +0800

    First Commit

---
+++ src/app/Userdashboard/UserCreateAccount/UserCreateAccount.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-user-create-account',
+    templateUrl: './UserCreateAccount.component.html',
+    styleUrl: './UserCreateAccount.component.scss'
+})
+
+export class UserCreateAccountComponent {
+  email: string = '';
+  password: string = '';
+
+  login() {
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+
+    //connect to backend
+  }
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { UserCreateAccountComponent } from './Userdashboard/UserCreateAccount/UserCreateAccount.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: 'login', component: UserCreateAccountComponent },
+];
